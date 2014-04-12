5d2a94b5efc7adae04f9e0841ab578fb0b562857
Martin Kaas <ggimkaas@gmail.com>
2014-04-12 14:11:48 +0300

    updating UI from thread, includes JSON decoding + untested & unimplemented http request code.

--- app/src/main/java/com/hesudi/reminderclient/app/AsyncItemAdapterUpdate.java
+++ app/src/main/java/com/hesudi/reminderclient/app/AsyncItemAdapterUpdate.java
@@ -3,6 +3,14 @@ package com.hesudi.reminderclient.app;
 import android.os.AsyncTask;
 import android.widget.GridView;
 
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -15,7 +23,8 @@ public class AsyncItemAdapterUpdate extends AsyncTask<List<Item>, List<Item>, Vo
     public AsyncItemAdapterUpdate(GridView gvItems) {
         m_gvItems = gvItems;
     }
-
+    private static final String m_serverAddress = "localhost:8080";
+    String jsonTest = "{data: \\r\\n\t[{name: 'test',\\r\\n\tstatus: 1},\\r\\n\t{name: 'test1',\\r\\n\tstatus: 0},\\r\\n\t{name: 'test2',\\r\\n\tstatus: 0},\\r\\n\t{name: 'test3',\\r\\n\tstatus: 1},\\r\\n\t{name: 'test4',\\r\\n\tstatus: 1},\\r\\n\t{name: 'test5',\\r\\n\tstatus: 0},\\r\\n]}";
     private ArrayList<Item> getSampleData() {
         ArrayList<Item> items = new ArrayList<Item>();
         items.add(new Item("Go to bathroom", 0));
@@ -26,18 +35,49 @@ public class AsyncItemAdapterUpdate extends AsyncTask<List<Item>, List<Item>, Vo
         items.add(new Item("pet the cat", 0));
         return items;
     }
+    private ArrayList<Item> getItems() {
+        return jsonDecode(jsonTest/*httpRequest(m_serverAddress)*/);
+    }
+    private String httpRequest(String url) {
+        try {
+            return EntityUtils.toString(
+                    (new DefaultHttpClient())
+                            .execute(new HttpGet(url))
+                            .getEntity()
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return httpRequest(url);
+    }
+
+    private ArrayList<Item> jsonDecode(String encoded) {
+        ArrayList<Item> items = new ArrayList<Item>();
+        try {
+            JSONArray jsonItems = (new JSONObject(encoded.replace("\\r\\n", " "))).getJSONArray("data");
+            for (int i = 0; i < jsonItems.length(); ++i) {
+                items.add(new Item(jsonItems.getJSONObject(i)));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return items;
+    }
+
     @Override
     protected Void doInBackground(List<Item>... lists) {
-        ArrayList<Item> items = getSampleData();
+        ArrayList<Item> items;
+
         while (true) {
             Random r = new Random();
             try {
+                items = getItems();
                 Integer randomId = r.nextInt(items.size());
                 Item old = items.get(randomId);
                 Item replacement = new Item(old.name, r.nextInt(2));
                 items.set(randomId, replacement);
                 publishProgress(items);
-                Thread.sleep(5000);
+                Thread.sleep(500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
--- app/src/main/java/com/hesudi/reminderclient/app/Item.java
+++ app/src/main/java/com/hesudi/reminderclient/app/Item.java
@@ -1,5 +1,8 @@
 package com.hesudi.reminderclient.app;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by Martin on 11-4-2014.
  */
@@ -7,6 +10,15 @@ public class Item {
     public String name;
     public Integer status;
     public Integer id;
+
+    public Item(JSONObject jsonItem) {
+        try {
+            name = jsonItem.getString("name");
+            status = jsonItem.getInt("status");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 
     public Item(String name, Integer status) {
         this.name = name;
